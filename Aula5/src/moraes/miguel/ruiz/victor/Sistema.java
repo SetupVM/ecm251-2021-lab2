a2dbadc3fd86a2fc31fdfbb16ef11356e98baa6a
Victor <searommrv@hotmail.com>
2021-04-05 16:22:48 -0300

    Construtor do Sistema criado

--- Aula5/src/moraes/miguel/ruiz/victor/Sistema.java
+++ Aula5/src/moraes/miguel/ruiz/victor/Sistema.java
@@ -1,7 +1,36 @@
 package moraes.miguel.ruiz.victor;
 
+import java.util.Scanner;
+
 public class Sistema {
+    private boolean executarSistema;
+    private Scanner scanner;
+
     public void executar(){
-        
+
+        int opcao;
+        while (executarSistema){
+            exibirMenu();
+            opcao = scanner.nextInt();
+        }
+    }
+
+    public Sistema() {
+        this.executarSistema = true;
+        this.scanner = new Scanner(System.in); //Cria um scanner para o teclado(entrada padrao)
+
+    }
+
+    private void exibirMenu() {
+        System.out.println("Bem Vindo!");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar dinheiro");
+        System.out.println("3 - Sacar dinheiro");
+        System.out.println("4 - Transferir dinheiro");
+        System.out.println("5 - Pagar conta (titulo)");
+        System.out.println("0 - Encerrar Sistema");
+
+
+
     }
 }
